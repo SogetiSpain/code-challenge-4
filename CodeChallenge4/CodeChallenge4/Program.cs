2aa4440bf3543db3a5f55ae3d94772573cd60131
Eduard Moret <eduard.moret-ruz@sogeti.com>
2016-02-18 19:09:32 +0100

    making the structure

--- CodeChallenge4/CodeChallenge4/Program.cs
+++ CodeChallenge4/CodeChallenge4/Program.cs
@@ -1,24 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace CodeChallenge4
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using ServiceLayer;
+
     class Program
     {
         static void Main(string[] args)
         {
-
+          
         }
+
+        
 
         public void ShowTable(IEnumerable<string> employees)
         {
-            Console.WriteLine("Name                |Position              |SeparationDate          ");
+            Console.WriteLine("Name    \t            |Position      \t         |SeparationDate        ");
             Console.WriteLine("--------------------------------------------------------------------");
 
+            //iteration
+            //Console.Write(string.Format(employee""));
             Console.WriteLine("--------------------|----------------------|------------------------");
+            //endItertion
 
         }
     }
