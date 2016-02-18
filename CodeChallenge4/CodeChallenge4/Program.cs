c8347797552e4e52cdfd6587ae1e54eef8287b73
Eduard Moret <eduard.moret-ruz@sogeti.com>
2016-02-18 18:03:40 +0100

    first commit

---
+++ CodeChallenge4/CodeChallenge4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeChallenge4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public void ShowTable(IEnumerable<string> employees)
+        {
+            Console.WriteLine("Name                |Position              |SeparationDate          ");
+            Console.WriteLine("--------------------------------------------------------------------");
+
+            Console.WriteLine("--------------------|----------------------|------------------------");
+
+        }
+    }
+}
